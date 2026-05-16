84872642cb3a939afc89594f640e8efce97702a5
Jones <joneswtf2026@github.com>
2026-05-16 17:01:57 -0300

    fix: emoji bandeira some com filtro (remove overflow:hidden do card); feat: adiciona FWC0

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const FWC_LABELS = {
   17:'🎶',18:'🌎',19:'🔥'
 };
 const SPECIALS = [
-  {id:"FWC",label:"FIFA World Cup History",emoji:"👟",nums:[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19]},
+  {id:"FWC",label:"FIFA World Cup History",emoji:"👟",nums:[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19]},
   {id:"CC", label:"Coca-Cola",             emoji:"🥤",nums:[1,2,3,4,5,6,7,8,9,10,11,12,13,14]},
 ];
 
